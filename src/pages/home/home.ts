1bd8ef7db9c32fe10d68ca06fc6aeabb1f40386f
SeyoonPark <psyoona@naver.com>
2017-05-13 23:08:57 +0900

    Ver 0.25 아이디 중복체크 기능

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { RegisterPage } from '../register/register';
 import 'rxjs/Rx';
@@ -11,8 +11,9 @@ import 'rxjs/Rx';
 export class HomePage {
   inputID: string;
   inputPW: string;
+  loginConfirm: string;
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, private toastCtrl: ToastController) {
 
   }
 
@@ -27,15 +28,29 @@ export class HomePage {
       this.http.post("http://117.17.158.192:8200/MainServer/servers", loginData)
                 //.map(data => data.json())
                 .subscribe(res => {
-                  // 서버로부터 결과값을 받는 부분
-                  console.log(res['_body']);
+                  // 서버로부터 결과 값을 받는 부분
+                  this.loginConfirm = res['_body'];
+                  if(this.loginConfirm == "success"){
+                    let toast = this.toastCtrl.create({
+                      message: '로그인이 성공',
+                      duration: 3000,
+                      position: 'middle'
+                    });
+                    toast.present();
+                    this.loginConfirm = null;
+                  }else{
+                    let toast = this.toastCtrl.create({
+                      message: '아이디 또는 비밀번호가 일치하지 않습니다.',
+                      duration: 3000,
+                      position: 'middle'
+                    });
+                    toast.present();
+                  }
         });
     }
     this.inputID = '';
     this.inputPW = '';
   }
-
-
 
   RegisterPage(){
     this.navCtrl.push(RegisterPage);
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { HomePage } from '../home/home';
+
 import 'rxjs/Rx';
 
 @Component({
@@ -12,11 +14,67 @@ export class RegisterPage {
   inputPW: string;
   nickname: string;
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  checkIDresult: string;
 
+
+  constructor(public navCtrl: NavController, public http: Http, private toastCtrl: ToastController) {
+    this.checkIDresult = "uncheck";
+  }
+
+  repetition(inputID){
+    // 아이디 중복 체크 버튼을 눌렀을 때
+    let checkID={
+      type: 'check',
+      id: inputID
+    }
+    if (inputID.trim() != '') {
+      this.http.post("http://117.17.158.192:8200/MainServer/servers", checkID)
+                //.map(data => data.json())
+                .subscribe(res => {
+                  this.checkIDresult = res['_body'];
+                  if(this.checkIDresult == "using"){
+                    //  중복된 아이디가 있는 경우 알리기 위한 Toast
+
+                    let toast = this.toastCtrl.create({
+                      message: '중복된 아이디가 존재합니다.',
+                      duration: 3000,
+                      position: 'middle'
+                    });
+
+                    toast.present();
+                  }else{
+                    // 중복된 아이디가 없는 경우 알리기 위한 Toast
+                    let toast = this.toastCtrl.create({
+                      message: '사용 가능한 아이디 입니다.',
+                      duration: 3000,
+                      position: 'middle'
+                    });
+                    toast.present();
+                  }
+        });
+    }
   }
 
   Submit(inputID, inputPW, nickname) {
+    // 회원가입 완료 버튼을 눌렀을 때
+    if(this.checkIDresult == "using"){
+      let toast = this.toastCtrl.create({
+        message: '중복된 아이디가 존재합니다. 진행할 수 없습니다.',
+        duration: 3000,
+        position: 'middle'
+      });
+      toast.present();
+      return ;
+    }else if(this.checkIDresult == "uncheck"){
+      let toast = this.toastCtrl.create({
+        message: '아이디 중복 체크를 하지 않았습니다. 진행할 수 없습니다.',
+        duration: 3000,
+        position: 'middle'
+      });
+      toast.present();
+      return ;
+    }
+
     let submitData={
       type: 'register',
       id: inputID,
@@ -30,13 +88,22 @@ export class RegisterPage {
                 .subscribe(res => {
                   console.log();
         });
+    }else{
+      // 작성되지 않은 부분이 있는 경우
+      let toast = this.toastCtrl.create({
+        message: '작성되지 않은 항목이 있습니다. 확인 바랍니다.',
+        duration: 3000,
+        position: 'middle'
+      });
+      toast.present();
+      return ;
     }
     this.inputID = '';
     this.inputPW = '';
     this.nickname = '';
   }
 
-  Cancel() {
-
+  Cancle() {
+    this.navCtrl.push(HomePage);
   }
 }
